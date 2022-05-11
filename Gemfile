cca143ff354de805cfb7673d497211a8af9edbd3
gehrig_barnes <gehrigbarnes@gmail.com>
2022-05-11 13:40:01 -0500

    set up front end and backend

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby "2.7.4"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.2", ">= 7.0.2.4"
 
+gem 'rack-cors'
+
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+##if you eventually want to move this to produciton, we have to change the orgins url to the URL of the produciton server we are using.
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "http://localhost:3001"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
