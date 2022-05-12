182420dadb7bc7cc26608046ad5e3bb5acb91664
J Lee <jay.j.lee.1@gmail.com>
2022-05-12 14:52:13 -0400

    initial backend setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :meals
+  resources :inventories
+  resources :activities
+  resources :user_trips
+  resources :trips
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
