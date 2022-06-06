acf8073ef69eb7569c6e0157dd34f18bfb207376
J Lee <jay.j.lee.1@gmail.com>
2022-06-06 15:48:33 -0400

    started footer

--- a-front-end/src/App.js
+++ a-front-end/src/App.js
@@ -1,35 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import React, { useEffect, useState } from "react";
-import { Box, Flex, Spacer, Text, Center, Square, Circle } from '@chakra-ui/react'
-import UIDrawer from './Components/UI/Drawer'
-
-
+import {
+  Box,
+  Flex,
+  Spacer,
+  Text,
+  Center,
+  Square,
+  Circle,
+  Image,
+  Stack,
+  Button,
+} from "@chakra-ui/react";
+import UIDrawer from "./Components/UI/Drawer";
 
 function App() {
   return (
-        
     <div>
       <UIDrawer></UIDrawer>
       <Box>
-      <Circle bg='tomato' color='white'>
-        {/* <Flex> */}
-      <Text fontSize="4xl">Welcome to CabinShare</Text>
-      {/* </Flex> */}
-      </Circle>
+        <Circle bg="tomato" color="white">
+          {/* <Flex> */}
+          <Text fontSize="4xl">Welcome to CabinShare</Text>
+          {/* </Flex> */}
+        </Circle>
       </Box>
-      <Spacer/>
+      <Spacer />
       <Box>
-      <Square bg='blue' color='white'>
-        {/* <Flex> */}
-      <Text fontSize="2xl">Your one-stop application for planning your next shared cabin weekend.</Text>
-      {/* </Flex> */}
-      </Square>
+        <Square bg="blue" color="white">
+          {/* <Flex> */}
+          <Text fontSize="2xl">
+            Your one-stop application for planning your next shared cabin
+            weekend.
+          </Text>
+          {/* </Flex> */}
+        </Square>
+        <Center>
+          <Image src="https://vancouverisland.travel/app/uploads/2021/04/wood-cabin-on-vancouver-island_BenGiesbrecht.jpg" />
+        </Center>
+        <Center>
+        <Stack spacing={4} direction="row" align="center">
+          <Button colorScheme="teal" size="md">
+            Trips
+          </Button>
+          <Button colorScheme="teal" size="md">
+            Activities
+          </Button>
+          <Button colorScheme="teal" size="md">
+            Meals
+          </Button>
+          <Button colorScheme="teal" size="md">
+            Inventories
+          </Button>
+        </Stack>
+        </Center>
       </Box>
-      </div>
-     
-
-      
+    </div>
   );
 }
 
