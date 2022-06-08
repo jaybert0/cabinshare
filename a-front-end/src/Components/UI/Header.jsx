2377068c329dd14cf99ed99afba423769bb8721f
J Lee <jay.j.lee.1@gmail.com>
2022-06-08 11:33:00 -0400

    cleaned up code for header... alpha done

--- a-front-end/src/Components/UI/Header.jsx
+++ a-front-end/src/Components/UI/Header.jsx
@@ -1,46 +1,36 @@
 import {
-    Box,
-    Container,
-    Flex,
-    Spacer,
-    Text,
-    Center,
-    Square,
-    Circle,
-    Image,
-    Stack,
-    Button,
-    Link
-  } from "@chakra-ui/react";
-  import logo from './cabin.png';
-  import UIDrawer from "./Drawer";
+  Box,
+  Container,
+  Flex,
+  Spacer,
+  Text,
+  Center,
+  Square,
+  Circle,
+  Image,
+  Stack,
+  Button,
+  Link,
+} from "@chakra-ui/react";
+import logo from "./cabin.png";
+import UIDrawer from "./Drawer";
 
+function Header() {
+  return (
+    <header>
+      <Box w="100%" bg="white">
+        <Flex>
+          <Box p="5">
+            <Image height="50px" objectFit="contain" src={logo} />
+          </Box>
+          <Spacer />
+          <Box p="5">
+            <UIDrawer />
+          </Box>
+        </Flex>
+      </Box>
+    </header>
+  );
+}
 
-  function Header() {
-    return (
-      <header>
-        <Box
-          w='100%'
-          bg="white"
-        //   color="white"
-        >
-          {/* <Container maxWidth="lg"> */}
-            <Flex 
-            // direction="row" 
-            // textAlign="center" pt={{ xs: 2, sm: 3 }} pb={{ xs: 2, sm: 3 }}
-            >
-              <Box p='5'>
-              <Image height='50px' objectFit='contain' src={logo}/>
-              </Box>
-              <Spacer />
-              <Box p='5'>
-              <UIDrawer/>
-              </Box>
-            </Flex>
-          {/* </Container> */}
-        </Box>
-      </header>
-    );
-  }
-
-  export default Header
+export default Header;
